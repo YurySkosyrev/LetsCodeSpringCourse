9b340ba6046a2efaf27fcb96a2e0e88b51bd0661
YurySkosyrev <skosyrev-87@mail.ru>
2022-09-13 18:31:18 +0300

    Database adding part 2

--- sweeter/src/main/java/com/example/sweeter/GreetingController.java
+++ sweeter/src/main/java/com/example/sweeter/GreetingController.java
@@ -1,14 +1,23 @@
 package com.example.sweeter;
 
+import com.example.sweeter.domain.Message;
+import com.example.sweeter.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -19,8 +28,40 @@ public class GreetingController {
 
     @GetMapping()
     public String main(Map<String, Object> model){
-        model.put("some", "Hello LetsCode");
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag,
+                      Map<String, Object> model){
+        Message message = new Message(text, tag);
+
+        messageRepo.save(message);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+
+        Iterable<Message> messages;
+
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+
+
 
 }
--- sweeter/src/main/java/com/example/sweeter/repository/MessageRepo.java
+++ sweeter/src/main/java/com/example/sweeter/repository/MessageRepo.java
@@ -3,7 +3,10 @@ package com.example.sweeter.repository;
 import com.example.sweeter.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
+    List<Message> findByTag(String tag);
 
 }
