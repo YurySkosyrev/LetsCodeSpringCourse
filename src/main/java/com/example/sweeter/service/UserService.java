5dc9d0cdf1584d9c9a65d813e5641174e6e45e9a
YurySkosyrev <skosyrev-87@mail.ru>
2022-10-06 13:54:10 +0300

    Step 9/20 Email send added

--- src/main/java/com/example/sweeter/service/UserService.java
+++ src/main/java/com/example/sweeter/service/UserService.java
@@ -1,10 +1,16 @@
 package com.example.sweeter.service;
 
+import com.example.sweeter.domain.Role;
+import com.example.sweeter.domain.User;
 import com.example.sweeter.repository.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 /**
  * Сервис для получения пользователей из БД
@@ -14,13 +20,57 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private final UserRepo userRepo;
+    private final MailSenderService mailSender;
 
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, MailSenderService mailSender) {
         this.userRepo = userRepo;
+        this.mailSender = mailSender;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDB = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDB != null){
+            return false;
+        }
+
+        user.setActive(false);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepo.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Sweater. Please, visit next link: " +
+                            "http://localhost:8090/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user == null){
+            return false;
+        }
+
+        user.setActivationCode(null);
+        user.setActive(true);
+        userRepo.save(user);
+
+        return true;
     }
 }
